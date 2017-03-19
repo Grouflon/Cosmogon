09a0523dba5ec9ca311c8981afbff9bdc688069c
Remi minitop <remi@uselessworks.com>
2017-03-19 20:17:33 +0100

    GameManager + planets name generator

---
+++ assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Planet planetPrefab;
+
+    public delegate void PlanetAction(Planet _planet);
+    public event PlanetAction planetAdded;
+
+    public Planet AddPlanet(string _name, Vector2 _position)
+    {
+        Planet p = Instantiate(planetPrefab, new Vector3(_position.x, _position.y, 0.0f), Quaternion.identity);
+        p.name = _name;
+
+        m_planets.Add(p);
+
+        if (planetAdded != null) planetAdded(p);
+
+        return p;
+    }
+
+	// Use this for initialization
+	void Start () {
+        m_planets = new List<Planet>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    List<Planet> m_planets;
+}
--- assets/Scripts/SystemGen.cs
+++ assets/Scripts/SystemGen.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class SystemGen : MonoBehaviour {
 
-    public Planet planetPrefab;
     public int nbPlanet;
-
-    public List<Planet> planets;
-
 
     // Use this for initialization
     void Start()
     {
+        GameManager gm = FindObjectOfType<GameManager>();
+        List<Planet> planets = new List<Planet>();
+
+        string vowels = "aeiou";
+        string consonants = "bcdfghjklmnpqrstvwxz";
+
         //planetPrefabs instantiations
         for (int i = 0; i < nbPlanet; i++)
         {
             //position entre -2 et 2 et -4 et 4 par pas de 0.5
             float x = 0.5f * Random.Range(0, 9) - 2;
             float y = 0.5f * Random.Range(0, 17) - 4;
-            Vector3 pos = new Vector3(x, y, 0);
 
-            //Instantiation de la planetPrefabe
-            Planet pla = Instantiate(planetPrefab, pos, Quaternion.identity);
-            pla.name = "planetPrefab" + i;
-            pla.SetMaxLinkCount(nbPlanet);
-            pla.armyCount = Random.Range(0, 4);
-            planets.Add(pla);
+            // Name generation
+            int nameLength = Random.Range(1, 6) * 2;
+            string planetName = "";
+            //int vowelConsonantSelector = Random.Range(0, 2); // outer bound is exclusive, so 2 and not 1
+            int vowelConsonantSelector = 1; // 0 is vowel, 1 is consonant
+            for (int l = 0; l < nameLength; ++l)
+            {
+                char nextChar;
+                if (vowelConsonantSelector == 0)
+                {
+                    nextChar = vowels[Random.Range(0, vowels.Length)];
+                }
+                else
+                {
+                    nextChar = consonants[Random.Range(0, consonants.Length)];
+                }
+                planetName += nextChar;
+                vowelConsonantSelector = (vowelConsonantSelector + 1) % 2;
+            }
+
+            // Add planet
+            Planet p = gm.AddPlanet(planetName, new Vector2(x, y));
+            p.SetMaxLinkCount(nbPlanet);
+            p.armyCount = Random.Range(0, 4);
+            planets.Add(p);
         }
 
 
