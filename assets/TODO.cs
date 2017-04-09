ab6f16f79963890654deef940658eaddc33c8782
Swann Martin-Raget <swann@swann.swann>
2017-04-09 20:04:38 +0200

    Tout doux

---
+++ assets/TODO.cs
@@ -0,0 +1,34 @@
+﻿/* 
+TODO
+    GAMEPLAY
+    - Reduire un peu la zone de reach des link
+    - Avoir des planètes donnant une ressource spéciale à chaque tour
+    - Skills: Trou noir = détruit une planète
+    - Skills: Conquer = Change la couleur d'une planète
+    - Skills: Teleport = Attaque une planète sans lien
+    - Skills: Firewall = Protège une planète 3 tours
+
+    UI
+    - Reduire la largeur de la selection de planète
+    - Pouvoir se déplacer hors screen avec double touch
+    - Faire un appui long pour voir la zone de reach apparaitre (appui court = sélection)
+
+    GENERATION
+    - Placer toujours les players le plus loin possible les uns des autres
+    - Interdire les planètes trop proche
+    - Interdire les planètes trop éloignées
+    - Positionnement moins "carré"
+
+    BUGS
+    - BUG: Il y a toujours deux planètes l'une sur l'autre lors de la génération
+
+    SOUND
+    - Fx de sélection et de création de links
+
+
+
+
+
+DONE
+    - Plein de trucs trop oufs
+*/
