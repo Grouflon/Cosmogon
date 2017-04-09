21be9c65a14d233d93e8d5c26e5c1f3b08aeeada
Swann Martin-Raget <swann@swann.swann>
2017-04-09 19:32:57 +0200

    Tutorial management and scene 2

---
+++ assets/Scripts/TutoSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TutoSceneManager : MonoBehaviour {
+
+    GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager=GameObject.Find("GameManager");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameManager.GetComponent<GameManager>().m_gameOver) {
+            if (SceneManager.GetActiveScene().name == "Tuto1")
+            {
+                SceneManager.LoadScene("Tuto2");
+            }
+            else if (SceneManager.GetActiveScene().name == "Tuto2")
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+}
